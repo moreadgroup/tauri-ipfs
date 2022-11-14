79f4e21c2ef3a008c3c4833960dc754c3f64b761
Conan Chan <conan8chan@yahoo.com>
2022-11-14 16:14:38 +0800

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,47 @@
+<script>
+  import Greet from "$lib/Greet.svelte";
+
+  import { onMount } from "svelte";
+
+  import * as IPFS from "ipfs-core";
+
+  onMount(async () => {
+    /* Your code here */
+
+    const ipfs = await IPFS.create();
+    const { cid } = await ipfs.add("Hello world");
+
+    console.info("ipfs test cid=", cid);
+    // QmXXY5ZxbtuYj6DnfApLiGstzPN7fvSyigrRee3hDWPCaf
+  });
+</script>
+
+<h1>Welcome to Tauri!</h1>
+
+<div class="row">
+  <a href="https://vitejs.dev" target="_blank">
+    <img src="/vite.svg" class="logo vite" alt="Vite Logo" />
+  </a>
+  <a href="https://tauri.app" target="_blank">
+    <img src="/tauri.svg" class="logo tauri" alt="Tauri Logo" />
+  </a>
+  <a href="https://kit.svelte.dev" target="_blank">
+    <img src="/svelte.svg" class="logo svelte" alt="Svelte Logo" />
+  </a>
+</div>
+
+<p>Click on the Tauri, Vite, and Svelte logos to learn more.</p>
+
+<div class="row">
+  <Greet />
+</div>
+
+<style>
+  .logo.vite:hover {
+    filter: drop-shadow(0 0 2em #747bff);
+  }
+
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00);
+  }
+</style>
